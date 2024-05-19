08ae377c05be1ac8ee7ea7b66dd3c3f7a020d340
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-19 15:36:30 +0500

    commit

---
+++ microsoft_clone/script.js
@@ -0,0 +1,12 @@
+
+
+
+function showSidebar(){
+    const sideBar = document.querySelector('#navbar')
+    sideBar.style.display = 'block'
+}
+
+function hideSidebar(){
+    const sideBar = document.querySelector('#navbar')
+    sideBar.style.display = 'none'
+}
